683fb7896094d14dc945212a0822044bbfba742c
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-12-27 12:28:36 +0100

    improve fileSize output by adding readable sizes

--- test1/file-stats.go
+++ test1/file-stats.go
@@ -15,11 +15,12 @@ func main() {
 		log.Fatal(err)
 	}
 
-	readableSize := utils.ByteCountBase2(stats.Size())
+	sizeBase2 := utils.ByteCountBase2(stats.Size())
+	sizeBase10 := utils.ByteCountBase10(stats.Size())
 
 	//Prints stats of the file
 	fmt.Printf("Permission: %s\n", stats.Mode())
 	fmt.Printf("Name: %s\n", stats.Name())
-	fmt.Printf("Size: %s\n", readableSize)
+	fmt.Printf("Size: %s / %s\n", sizeBase2, sizeBase10)
 	fmt.Printf("Modification Time: %s\n", stats.ModTime())
 }
--- utils/byte-count.go
+++ utils/byte-count.go
@@ -19,20 +19,21 @@ func ByteCountBase10(b int64) string {
 		div *= unit
 		exp++
 	}
-	return fmt.Sprintf("%.1f %cB",
+	return fmt.Sprintf("%.2f %cB",
 		float64(b)/float64(div), "kMGTPE"[exp])
 }
 
-func ByteCountBase2(b int64) string {
+func ByteCountBase2(byteCount int64) string {
 	const unit = 1024
-	if b < unit {
-		return fmt.Sprintf("%d B", b)
+
+	if byteCount < unit {
+		return fmt.Sprintf("%d B", byteCount)
 	}
-	div, exp := int64(unit), 0
-	for n := b / unit; n >= unit; n /= unit {
+	div, exp := unit, 0
+	for n := byteCount / unit; n >= unit; n /= unit {
 		div *= unit
 		exp++
 	}
-	return fmt.Sprintf("%.1f %ciB",
-		float64(b)/float64(div), "KMGTPE"[exp])
+	return fmt.Sprintf("%.2f %ciB",
+		float64(byteCount)/float64(div), "KMGTPE"[exp])
 }
