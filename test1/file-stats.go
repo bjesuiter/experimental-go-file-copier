4082e180414e3d14ccaafe3ad0827fa26894d7f1
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-12-27 11:57:52 +0100

    add simple function to get file stats

--- test1/file-stats.go
+++ test1/file-stats.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	stats, err := os.Stat("example/source/image.jpg")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Prints stats of the file
+	fmt.Printf("Permission: %s\n", stats.Mode())
+	fmt.Printf("Name: %s\n", stats.Name())
+	fmt.Printf("Size: %d\n", stats.Size())
+	fmt.Printf("Modification Time: %s\n", stats.ModTime())
 }
